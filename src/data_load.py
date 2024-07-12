b85037b4ccbb97c52ef859c3d57adf072140fbec
fonseca-matheus <matheusrdf1997@hotmail.com>
2024-07-12 07:48:34 -0400

    att classe DataLoad

--- src/data_load.py
+++ src/data_load.py
@@ -17,21 +17,21 @@ class DataLoad:
         df_historical = pd.DataFrame(historical_excel)
 
         '''
-        Aplicar logica sobre a quantidade de linhas.
-        Se for menor que self.completed_dest, ele aplica o rollback, caso contrario autoriza a concatenacao dos dataframes.
+            Aplicar logica sobre a quantidade de linhas.
+            Se for menor que self.completed_dest, ele aplica o rollback, caso contrario autoriza a concatenacao dos dataframes.
         '''
         total_lines = df_completed.shape[0] + df_historical.shape[0]
         if df_historical.shape[0] > total_lines:
-            # Lógica para rollback
-            print("Rollback: A operação foi cancelada devido a um problema na quantidade de linhas.")
+            #Lógica para rollback
+            print("A operação foi cancelada devido a um problema na quantidade de linhas!")
         else:
-            # Lógica para concatenação
+            #Lógica para concatenação
             df_concatenated = pd.concat([df_historical, df_completed], ignore_index=True)
             df_concatenated.to_excel(self.completed_dest, index=False)
-            print("Concatenação: Os dados foram concatenados e salvos com sucesso.")
+            print("Os dados foram concatenados e salvos com sucesso!")
 
-        #DATAFRAME CARTÕES EM ABERTO
+        #DATAFRAME CARTÕES EM ABERTO/FAZER/ATRASADO
         df_n_completed = self.df.loc[self.df['id_lista'] != f'{LIST_ID}']
-        df1_concatened = pd.concat([df_n_completed, df_concatenated])
-        df1_concatened.to_excel(self.dest, index=False)
+        df_concatenated1 = pd.concat([df_n_completed, df_concatenated])
+        df_concatenated1.to_excel(self.dest, index=False)
 
