ae83f00907f7d67cdb6051746e3d6b5b99046bf7
fonseca-matheus <matheusrdf1997@hotmail.com>
2024-07-16 14:29:29 -0400

    att classe DataArchive

--- src/data_archive.py
+++ src/data_archive.py
@@ -7,14 +7,17 @@ from src.envs import API_KEY, API_TOKEN, LIST_ID
 
 class DataArchive:
     def __init__(self, card_ids: List[Dict[str, Any]]) -> None:
-        self.completed_excel = r'C:\Users\matheus.rabelo\BioSeg Segurança do Trabalho\Indicadores de Gestão - DataWarehouse - DATA WAREHOUSE\TRELLO\GESTÃO TÉCNICA CONCLÚIDOS.xlsx'
         self.cards = card_ids
  
     def archive_cards(self) -> None:
         for card in self.cards:
             if card['id_lista'] == f'{LIST_ID}':
                 url = f"https://api.trello.com/1/cards/{card['id']}/closed"
-                params = {'key': API_KEY, 'token': API_TOKEN, 'value': 'true'} # 'true' para arquivar o cartão  
+                params = {
+                    'key': API_KEY, 
+                    'token': API_TOKEN, 
+                    'value': 'true' #'true' para arquivar o cartão
+                }   
                 try:
                     response = requests.put(url, params=params)
                     response.raise_for_status()
